fe2d2e80aba141aebcbb7f8bb54bcded0ce26140
TechCorp07 <poso.draxy@gmail.com>
2022-03-10 16:36:08 -0800

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,4 @@
 #include <stdio.h>
-
 /**
  *  main - prints the puts message
  * Retrun 0 is answer is correct
@@ -7,5 +6,5 @@
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
-      	return (0);
+	return (0);
 }
