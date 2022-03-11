432f84a74b2322697c848aa1cc9b6d75d4ddd2e5
TechCorp07 <poso.draxy@gmail.com>
2022-03-11 15:00:33 -0800

    task 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - prints the alphabet from 0 to 9
+ *
+ *  Return: gives zero if program is good
+ */
+int main(void)
+{
+	int ch = '0';
+
+	while (ch <= '9')
+	{
+		putchar(ch);
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
