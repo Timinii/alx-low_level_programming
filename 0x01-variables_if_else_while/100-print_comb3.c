c18fda70e4b5a0beb1818df00d7a09472cff5a31
TechCorp07 <poso.draxy@gmail.com>
2022-03-11 15:50:42 -0800

    task 10

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of two digits without repeats
+ *
+ *  Return: gives zero if program is good
+ */
+int main(void)
+{
+	int ch = '0';
+	int cH = '1';
+
+	while (ch <= '9')
+	{
+		while (cH <= '9')
+		{
+			if (ch != cH)
+			{
+				putchar(ch);
+				putchar(cH);
+				putchar(',');
+				putchar(' ');
+			}
+			cH++;
+		}
+		ch++;
+		cH = '1';
+	}
+	putchar('\n');
+	return (0);
+}
