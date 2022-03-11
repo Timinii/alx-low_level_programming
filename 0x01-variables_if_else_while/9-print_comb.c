971857895d30bdc4e1b8af17126d41dd311a6f2d
TechCorp07 <poso.draxy@gmail.com>
2022-03-11 15:28:59 -0800

    task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - prints the numbers from 0 to 9 separated by ,
+ *
+ *  Return: gives zero if program is good
+ */
+int main(void)
+{
+	char ch = '0';
+
+	while (ch <= '9')
+	{
+		putchar(ch);
+		if (ch != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+		ch++;
+	}
+	putchar('\n');
+	return (0);
+}
