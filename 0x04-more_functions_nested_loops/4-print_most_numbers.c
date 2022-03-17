ead553a3229f7f519364fa2a53d08f9ac48dc380
TechCorp07 <poso.draxy@gmail.com>
2022-03-17 14:11:23 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -7,11 +7,15 @@
  */
 void print_most_numbers(void)
 {
-	char text[9] = "01356789";
+	char text[11] = "0123456789";
 	int i = 0;
 
-	for (i = 0; i < 9; i++)
+	for (i = 0; i < 11; i++)
 	{
+		if (i == 2 || i == 4 )
+		{
+			i++;
+		}
 		_putchar(text[i]);
 	}
 	_putchar('\n');
---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _putchar - prints numbers from 0 to 9
+ *
+ * Return: Always 0.
+ */
+int _putchar(char c)
+{
+	return (c);
+}
