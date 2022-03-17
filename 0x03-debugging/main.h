a31918c8c4a1ac882d5bb5c1ff4a90646e80877a
Einstein <TechCorp07@users.noreply.github.com>
2022-03-17 13:56:31 +0200

    Update main.h

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -3,6 +3,33 @@
 
 #include <stdio.h>
 
+/**
+ * largest_number - returns the largest of 3 numbers
+ * @a: first integer
+ * @b: second integer
+ * @c: third integer
+ * Return: largest number
+ */
 void positive_or_negative(int i);
+
+int largest_number(int a, int b, int c)
+{
+    int largest;
+
+    if (a > b && b > c)
+    {
+        largest = a;
+    }
+    else if (b > a && a > c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
+
+    return (largest);
+}
 
 #endif /* MAIN_H */
