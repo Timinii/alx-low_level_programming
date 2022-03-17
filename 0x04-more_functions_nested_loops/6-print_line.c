faa011b7ca44131e66b1c357ba094d87c7d0758d
TechCorp07 <poso.draxy@gmail.com>
2022-03-17 14:50:19 -0700

    task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_line - draws a straight line
+ *@n: integer digit
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+	int i;
+
+	if ((n <= 0) != true)
+	{
+		for (i = 0; i < n; i++)
+			_putchar('_');
+	}
+	_putchar('\n');
+}
